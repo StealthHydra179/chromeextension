8398ee3720727ff2eb16e4e9c9e1e3b42cd5eec7
StealthHydra179 <47306082+StealthHydra179@users.noreply.github.com>
2023-07-04 22:37:15 -0400

    template code

--- background_worker/background.js
+++ background_worker/background.js
@@ -2,8 +2,8 @@ let tabList = []
 let changedSchema = false
 
 //script on all tabs when extension is created
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.local.get("tabList", function(result) {
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.local.get("tabList", function (result) {
         if (result.tabList && !changedSchema) {
             tabList = result.tabList
             console.log("tabList loaded from storage: ")
@@ -14,10 +14,10 @@ chrome.runtime.onInstalled.addListener(function() {
         //update tabList with current tab info
         chrome.tabs.query(
             queryInfo = {},
-            callback = function(tabArray) {
+            callback = function (tabArray) {
                 tabArray.forEach(tab => {
                     chrome.scripting.executeScript({
-                        target: {tabId: tab.id},
+                        target: { tabId: tab.id },
                         files: ['background_worker/injected_content.js']
                     }).then(() => {
                         console.log("injected content script into all tabs")
@@ -31,11 +31,44 @@ chrome.runtime.onInstalled.addListener(function() {
     })
 })
 
+chrome.runtime.onStartup.addListener(function () {
+    //same as installed
+    chrome.storage.local.get("tabList", function (result) {
+        if (result.tabList && !changedSchema) {
+            tabList = result.tabList
+            console.log("tabList loaded from storage: ")
+            console.log(tabList)
+        } else {
+            console.log("tabList not loaded from storage")
+        }
+        //update tabList with current tab info
+        chrome.tabs.query(
+            queryInfo = {},
+            callback = function (tabArray) {
+                tabArray.forEach(tab => {
+                    chrome.scripting.executeScript({
+                        target: { tabId: tab.id },
+                        files: ['background_worker/injected_content.js']
+                    }).then(() => {
+                        console.log("injected content script into all tabs")
+                    }).catch(err => {
+                        console.log(err)
+                        console.log(tab.url)
+                    })
+                })
+            }
+        )
+    })
+})
+
+chrome.runtime.onSuspend.addListener(function () {
+
+})
 
 //when new tab is created
-chrome.tabs.onCreated.addListener(function(tab) {
+chrome.tabs.onCreated.addListener(function (tab) {
     chrome.scripting.executeScript({
-        target: {tabId: tab.id},
+        target: { tabId: tab.id },
         files: ['background_worker/injected_content.js']
     }).then(() => {
         // console.log("injected content script into new tab")
@@ -43,10 +76,10 @@ chrome.tabs.onCreated.addListener(function(tab) {
 })
 
 //when tab is updated
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status === "complete") {
         chrome.scripting.executeScript({
-            target: {tabId: tab.id},
+            target: { tabId: tab.id },
             files: ['background_worker/injected_content.js']
         }).then(() => {
             // console.log("injected content script into updated tab")
@@ -57,10 +90,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 //chrome:// tabs don't work
 
 //get current state of the tabs and store it in tabList
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    // console.log("tab: " + sender + "\nmessage: " + request.message)
-    // console.log(sender);
-
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.message.state === "loaded") {
         //if object exists, update it
         //if object doesn't exist, add it
@@ -69,15 +99,15 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             if (tab.document_id === sender.documentId) {
                 tabExists = true
                 tab.visibility = "hidden"
-                tab.update_time.push({"visibility": "hidden","time": request.message.update_time})
-                tab.loaded_time.push({"state": "loaded","time": request.message.update_time})
+                tab.update_time.push({ "visibility": "hidden", "time": request.message.update_time })
+                tab.loaded_time.push({ "state": "loaded", "time": request.message.update_time })
                 tab.open = true
             }
         }
         )
         if (!tabExists) {
             tabList.push({
-                "document_id": sender.documentId, 
+                "document_id": sender.documentId,
                 "origin": sender.origin,
                 "url": sender.url,
                 // "document_url": sender.origin,
@@ -86,8 +116,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
                 "active": sender.tab.active,
                 "audible": sender.tab.audible,
                 "muted": sender.tab.mutedInfo.muted,
-                "update_time": [{"visibility": "hidden","time": request.message.update_time}],
-                "loaded_time": [{"state": "loaded","time": request.message.update_time}],
+                "update_time": [{ "visibility": "hidden", "time": request.message.update_time }],
+                "loaded_time": [{ "state": "loaded", "time": request.message.update_time }],
                 "open": true
             })
         }
@@ -98,8 +128,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             if (tab.document_id === sender.documentId) {
                 tab.open = false
                 tab.last_update_time = request.message.update_time
-                tab.update_time.push({"visibility": "hidden", "time": request.message.update_time})
-                tab.loaded_time.push({"state": "closed", "time": request.message.update_time})
+                tab.update_time.push({ "visibility": "hidden", "time": request.message.update_time })
+                tab.loaded_time.push({ "state": "closed", "time": request.message.update_time })
             }
         })
     }
@@ -108,7 +138,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         tabList.forEach(tab => {
             if (tab.document_id === sender.documentId && request.message.update_time >= tab.last_update_time) {
                 tab.visibility = "visible"
-                tab.update_time.push({"visibility": "visible", "time": request.message.update_time})
+                tab.update_time.push({ "visibility": "visible", "time": request.message.update_time })
             }
         })
     }
@@ -116,7 +146,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         tabList.forEach(tab => {
             if (tab.document_id === sender.documentId && request.message.update_time >= tab.last_update_time) {
                 tab.visibility = "hidden"
-                tab.update_time.push({"visibility": "hidden", "time": request.message.update_time})
+                tab.update_time.push({ "visibility": "hidden", "time": request.message.update_time })
             }
         })
     }
@@ -124,6 +154,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     console.log(request.message)
 
     updateStorage();
+    generateSpecifics();
     /*
     example sender object:
         documentId: "7DF44B79F740FEDBB3300A015FCA347D"
@@ -160,7 +191,38 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 //update history
 function updateStorage() {
-    chrome.storage.local.set({"tabList": tabList}, function() {
+    chrome.storage.local.set({ "tabList": tabList }, function () {
         console.log(tabList)
     })
 }
+
+function generateSpecifics() {
+    let specificList = {}
+    chrome.storage.local.get("specificList", function (result) {
+        if (result.specificList !== undefined) {
+            specificList = result.specificList
+        }
+        calculateSpecifics()
+    })
+
+    function calculateSpecifics() {
+        //tablist is the input
+        //specificList is the output
+
+        //specific list is a dictionary of dictionaries
+        /* the first key is "url" (sorted by the origns of the tabs)
+        the second key is "page" (sorted by the individual pages)
+
+        */
+        
+        saveSpecifics()
+    }
+    
+    function saveSpecifics() {
+        chrome.storage.local.set({ "specificList": specificList }, function () {
+            console.log(specificList)
+        })
+    }
+}
+
+
---
+++ popup/popup.js
@@ -0,0 +1,4 @@
+// onclick of the button in the html file
+document.getElementById('open_in_new_tab').addEventListener('click', function () {
+    chrome.tabs.create({ url: 'client/index.html' });
+})
